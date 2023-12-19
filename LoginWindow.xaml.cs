d2019838bc6a8e0be33317fdc26643edfe65d024
USER <USER@DESKTOP-8LRHPT1>
2023-12-19 13:07:43 +0400

    Создан интерфейс окна входа

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace SystemForGasEguipment
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для LoginWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
         }
