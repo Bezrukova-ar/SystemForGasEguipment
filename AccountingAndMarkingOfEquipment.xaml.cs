ec85d78c69e35266cba26e322f08711aac614952
USER <USER@DESKTOP-8LRHPT1>
2023-12-20 14:17:50 +0400

    Создана форма учета и маркировки оборудования

---
+++ AccountingAndMarkingOfEquipment.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemForGasEguipment
+{
+    /// <summary>
+    /// Логика взаимодействия для AccountingAndMarkingOfEquipment.xaml
+    /// </summary>
+    public partial class AccountingAndMarkingOfEquipment : Window
+    {
+        public AccountingAndMarkingOfEquipment()
+        {
+            InitializeComponent();
+        }
+    }
+}
